15b34b54ab7d69e4e0b85f0ef87a42cab3a503c3
tatsumack <tatsu.mack@gmail.com>
2019-08-03 23:02:42 +0900

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -15,3 +15,13 @@ mv ./alp /usr/local/bin
 cd /var/tmp && wget https://www.percona.com/downloads/percona-toolkit/3.0.13/binary/tarball/percona-toolkit-3.0.13_x86_64.tar.gz
 tar -zxvf percona-toolkit-3.0.13_x86_64.tar.gz
 mv ./percona-toolkit-3.0.13/bin/pt-query-digest /usr/local/bin
+
+# ssh
+cat << _EOS > /tmp/authorized_keys
+ssh-rsa hogehoge
+_EOS
+sudo cat /home/isucon/.ssh/authorized_keys >> /tmp/authorized_keys
+sudo mkdir -p /home/isucon/.ssh
+sudo mv /tmp/authorized_keys /home/isucon/.ssh/
+sudo chown -R isucon.isucon /home/isucon/.ssh/
+sudo chmod 600 /home/isucon/.ssh/authorized_keys
