ded46f8fe73011751a3fd49b8d7a456143aa2b73
tatsumack <tatsu.mack@gmail.com>
2020-10-02 22:01:49 +0900

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -ex
 
-sudo apt-get install -y nginx dstat unzip
-
-# nginx
-# sudo systemctl enable nginx
-# sudo systemctl start nginx
+sudo apt-get install -y dstat unzip
 
 # alp
 cd /var/tmp/ && wget https://github.com/tkuchiki/alp/releases/download/v0.4.0/alp_linux_amd64.zip
@@ -16,6 +12,11 @@ sudo mv ./alp /usr/local/bin
 cd /var/tmp && wget https://www.percona.com/downloads/percona-toolkit/3.0.13/binary/tarball/percona-toolkit-3.0.13_x86_64.tar.gz
 tar -zxvf percona-toolkit-3.0.13_x86_64.tar.gz
 sudo mv ./percona-toolkit-3.0.13/bin/pt-query-digest /usr/local/bin
+
+# slackcat
+cd /var/tmp && wget https://github.com/bcicen/slackcat/releases/download/v1.6/slackcat-1.6-linux-amd64
+sudo mv slackcat-1.6-linux-amd64 /usr/local/bin/slackcat
+sudo chmod +x /usr/local/bin/slackcat
 
 # ssh
 cat << _EOS > /tmp/authorized_keys
